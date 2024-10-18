ad8e898c11230f5dd84fe32540def36ad55223ac
zf-wei <zfwei11@gmail.com>
2024-10-18 15:13:08 -0700

    Sparse graph

---
+++ A1_lfr_gen.py
@@ -0,0 +1,86 @@
+# Set the range for mixing parameters
+import numpy as np
+
+num_processor = 20
+sample_count = 50
+
+step_total = 10
+step_size = 0.05
+MU = np.around(np.arange(step_size, step_size * step_total + 0.01, step_size), decimals=2)
+
+# Specify Parameters
+n = 1000
+tau1 = 2  # Power-law exponent for the degree distribution
+tau2 = 1.1  # Power-law exponent for the community size distribution
+avg_deg = 25  # Average Degree
+max_deg = int(0.1 * n)  # Max Degree
+min_commu = 60  # Min Community Size
+max_commu = int(0.1 * n)  # Max Community Size
+
+
+import os
+import pickle
+import networkx as nx
+from networkx.generators.community import LFR_benchmark_graph
+
+
+def gene_lfr_graph_mu(mu, sample_count=sample_count, output_dir='graph'):
+    """
+    Generates LFR benchmark graphs for a given mixing parameter (mu) and saves the graphs and memberships to a file.
+
+    Parameters:
+        mu (float): Mixing parameter.
+        sample_count (int): Number of graph samples to generate.
+        output_dir (str): Directory where the generated graphs and memberships are saved.
+    """
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+
+    graphs = []
+    memberships = []
+
+    for i in range(sample_count):
+        G = LFR_benchmark_graph(
+            n, tau1, tau2, mu, average_degree=avg_deg, max_degree=max_deg,
+            min_community=min_commu, max_community=max_commu, seed=2024
+        )
+
+        # Convert to undirected graph and remove self-loops
+        G = nx.Graph(G)
+        G.remove_edges_from(nx.selfloop_edges(G))
+
+        # Get the intrinsic communities and memberships
+        intrinsic_communities = {frozenset(G.nodes[v]["community"]) for v in G}
+        intrinsic_membership = {}
+        for node in range(G.number_of_nodes()):
+            for index, inner_set in enumerate(intrinsic_communities):
+                if node in inner_set:
+                    intrinsic_membership[node] = index
+                    break
+        intrinsic_membership = list(intrinsic_membership.values())
+
+        # Append the graph and membership to the lists
+        graphs.append(G)
+        memberships.append(intrinsic_membership)
+
+    # Save all graphs and memberships into a single file
+    combined_data = {
+        'graphs': graphs,
+        'memberships': memberships
+    }
+
+    mu_str = f"{mu:.2f}"
+    file_path = os.path.join(output_dir, f'lfr_graph_mu{mu_str}.pickle')
+    with open(file_path, 'wb') as file:
+        pickle.dump(combined_data, file)
+
+    print(f'Saved all graphs and memberships for mu={mu} to {file_path}')
+
+
+# Generate LFR graphs in parallel using ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+
+if __name__ == "__main__":
+    num_workers = num_processor  # 指定 worker 的数量
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        results = list(executor.map(gene_lfr_graph_mu, MU))
---
+++ A2_sparse.py
@@ -0,0 +1,95 @@
+import argparse
+import os
+import sys
+import pickle
+import numpy as np
+import networkx as nx
+
+sys.stdout.flush()
+
+num_workers = 2#0
+#sample_count = 3
+
+# 将子目录添加到 sys.path
+current_dir = os.getcwd()
+sys.path.append(os.path.join(os.path.join(current_dir), 'EffectiveResistanceSampling'))
+from EffectiveResistanceSampling.Network import *
+
+sys.path.append(os.path.join(os.path.join(current_dir), 'utilities'))
+from utilities.tools import *
+
+
+def sparse_lfr_graph_mu(mu, graph_type, epsilon=0.1, output_dir='graph_sparse'):
+    """Process a specific mixing parameter (mu) to calculate scores and quadratic forms."""
+    graphs, memberships = load_graph(mu, graph_type)
+    sample = 3#len(graphs)
+
+    graph_sparse = []
+
+    for i in range(sample):
+        G = graphs[i]
+        intrinsic_membership = memberships[i]
+        K = len(np.unique(intrinsic_membership))
+
+        edge_list = list(G.edges())
+        edge_list = np.array(edge_list)
+
+        edge_weights = nx.get_edge_attributes(G, 'weight')
+        edge_weights = np.array(edge_weights)
+        edge_weights = np.array([edge_weights[edge] if edge in edge_weights else 1 for edge in edge_list])
+
+        Gn = Network(edge_list, edge_weights)
+        Effective_R = Gn.effR(epsilon, 'spl')
+
+        while True:
+            Gn_Sparse = Gn.spl(int(G.number_of_edges() * 0.9), Effective_R, seed=2024)  # 第一个参数是 q 是保留的边的数量
+            G_sparse = to_networkx(Gn_Sparse)
+            if nx.is_connected(G_sparse):
+                break
+
+        print(i)
+        graph_sparse.append(G_sparse)
+
+    # Save all graphs and memberships into a single file
+    combined_data = {
+        'graphs': graph_sparse,
+    }
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    mu_str = f"{mu:.2f}"
+    file_path = os.path.join(output_dir, f'{graph_type}_graph_sparse_mu{mu_str}.pickle')
+    with open(file_path, 'wb') as file:
+        pickle.dump(combined_data, file)
+
+    print(f'Saved all graphs and memberships for mu={mu} to {file_path}')
+
+
+# Generate LFR graphs in parallel using ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Community detection on sparse and original networks.")
+    parser.add_argument('--graph_type', type=str, choices=['ppm', 'lfr'], default='ppm',
+                        help="Random graph type (ppm or lfr)")
+    parser.add_argument('--start_step', type=float, default=0.05, help="start_step")
+
+    args = parser.parse_args()
+    graph_type = args.graph_type
+    start_step = args.start_step
+
+    if graph_type == "ppm":
+        end_step = 0.9
+    elif graph_type == "lfr":
+        end_step = 0.5
+    step_size = 0.05
+    MU = np.around(np.arange(start_step, end_step + 0.01, step_size), decimals=2)
+
+
+    def process_mu(mu):
+        return sparse_lfr_graph_mu(mu, graph_type)
+
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        results = list(executor.map(process_mu, MU))
+
+
